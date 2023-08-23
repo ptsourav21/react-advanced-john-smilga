eb5a13bb217e143725d4d89355db0cf6e5f39c01
Purnendu Talukder <ptsourav21@gmail.com>
2023-08-24 00:39:14 +0600

    UseState Basics
    Click a button to change the title

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState('Hello World'));
+  // const value = useState(1)[0];
+  // const handler = useState(1)[1]
+  // console.log(value, handler);
+  const [text, setText] = useState('Random Title')
+  const handleClick = () =>{
+    if(text=='Random Title'){
+      setText('Hello World');
+    }else{
+      setText('Random Title');
+    }
+  }
+  return (
+    <>
+      <h2>{text}</h2>
+      <button className='btn' onClick={handleClick}>Change Title</button>
+    </>
+  );
 };
 
 export default UseStateBasics;
