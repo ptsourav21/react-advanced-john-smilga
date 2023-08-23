b645cbeedb4e447c897aaa11cbb4dcf1b3b322a1
Purnendu Talukder <ptsourav21@gmail.com>
2023-08-24 00:45:33 +0600

    Update 2-useState-basics.js

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,22 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+//use
+//component name must be uppercase
+//must be in the function/component body
+//can not call conditionally
 
 const UseStateBasics = () => {
   // console.log(useState('Hello World'));
   // const value = useState(1)[0];
   // const handler = useState(1)[1]
   // console.log(value, handler);
-  const [text, setText] = useState('Random Title')
-  const handleClick = () =>{
-    if(text=='Random Title'){
-      setText('Hello World');
-    }else{
-      setText('Random Title');
+
+  const [text, setText] = useState("Random Title");
+  const handleClick = () => {
+    if (text == "Random Title") {
+      setText("Hello World");
+    } else {
+      setText("Random Title");
     }
-  }
+  };
   return (
     <>
       <h2>{text}</h2>
-      <button className='btn' onClick={handleClick}>Change Title</button>
+      <button className="btn" onClick={handleClick}>
+        Change Title
+      </button>
     </>
   );
 };
